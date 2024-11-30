2bb68b6af0d8bf9f2a355c489f47add2b3ca92bc
Dani <danielgg04@gmail.com>
2024-11-30 22:21:34 +0100

    ya sabe cuando ha ganado un jugador. Da problemas si se clica demasiado rápido

--- game.js
+++ game.js
@@ -43,6 +43,67 @@ class TicTacToe {
     delay(miliseconds) {
         return new Promise(resolve => setTimeout(resolve,miliseconds))
     }
+
+    winner_I(square, letter){
+        // ganar por horizontal
+        const row_index = Math.floor(square / 3);
+        let row = this.board.slice(row_index*3,(row_index+1)*3);
+        for (let i=0; i < row.length; i++){
+            if (row[i]!=letter){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    winner_II(square, letter) {
+        //ganar por vertical
+        const col_index = square%3;
+        let col = [];
+        for (let i=0; i<3; i++){
+            col.push(this.board[i*3+col_index]);
+        }
+        for (let i=0; i < col.length; i++){
+            if (col[i]!=letter){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    winner_III(square, letter) {
+        //ganar por daigonal
+        const diagonal1_spots = [0,4,8];
+        const diagonal2_spots = [2,4,6];
+        let diagonal1=[];
+        let diagonal2=[];
+        for (let i=0; i<3; i++){
+            diagonal1.push(this.board[diagonal1_spots[i]]);
+            diagonal2.push(this.board[diagonal2_spots[i]]);
+        }
+        console.log(diagonal1);
+        console.log(diagonal2);
+
+        let win1 = true;
+        let win2 = true;
+        for (let i=0; i<3; i++){
+            if (diagonal1[i]!=letter){
+                win1 = false;
+                break;
+            }
+        }
+        for (let i=0; i<3; i++){
+            if (diagonal2[i]!=letter){
+                win2 = false;
+                break;
+            }
+        }
+        if (win1==true || win2==true){
+            return true;
+        }
+        return false;
+    }
+
 }
 
 export default TicTacToe;
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { RandomComputerPlayer, HumanPlayer } from './players.js';
 import TicTacToe from "./game.js";
 
-let x_player = new RandomComputerPlayer('x');
-let o_player = new RandomComputerPlayer('o');
+let x_player = new HumanPlayer('x');
+let o_player = new HumanPlayer('o');
 let game = new TicTacToe();
 
 play(game, x_player, o_player);
@@ -17,15 +17,23 @@ async function play(game, x_player, o_player){
     let letter = 'x';
 
     while (game.empty_squares() == true) {
-        // espera medio segundo
-        await game.delay(500);
-        console.log(game.available_moves());
 
         // se espera a que el jugador elija con await
         // se printea su eleccion
         let position = await player.get_move(button_list, game.available_moves());
         //el movimiento se guarda en la lista del tablero
         game.save_move(position, letter);
+
+
+
+        if (game.winner_I(position, letter)==true || game.winner_II(position, letter)==true || game.winner_III(position, letter)==true){
+            alert(`ha ganado el jugador ${letter}!`);
+            //return letter;
+
+        }
+
+        // espera medio segundo
+        //await game.delay(500);
 
         //cambio de turno
         if (letter=='x'){
@@ -36,8 +44,5 @@ async function play(game, x_player, o_player){
             letter = 'x';
             player = x_player;
         }
-    }
-    console.log(game.available_moves());
-    
-
+    }   
 }
